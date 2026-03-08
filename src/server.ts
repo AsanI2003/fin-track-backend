e552f135c5817edf883d7327fd6f15a9b88ee9cd
Asan Indusara <asanindusara348@gmail.com>
2026-03-08 19:06:50 +0530

    last commit: Added project files

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import authRoutes from "./routes/auth";
+import { authMiddleware } from "./middleware/authMiddleware";
+import expensesRoutes from "./routes/expenses";
+import billsRoutes from "./routes/bills"
+import aiRoutes from "./routes/ai";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI!, {})
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/expenses", expensesRoutes);
+app.use("/api/bills",billsRoutes);
+app.use("/api/ai", aiRoutes);
+
+
+app.get("/", (req, res) => res.send("API running"));
+
+app.listen(process.env.PORT!, () => console.log("Server running"));
